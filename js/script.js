5efc966bb7ff81b2e77e2541f14991e955ce1712
Mcworter <38241833+Mcworter09@users.noreply.github.com>
2018-10-10 14:18:40 +0900

    2nd Commit

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 new Chart(document.getElementById("skills"), {
     type: "doughnut",
     data: {
-        labels: ["PHP","Mysql","Javascript","HTML/CSS",],
+        labels: ["PHP","HTML/CSS", "Mysql","Javascript",],
         datasets: [
             {
-                data: [50, 20, 15, 15,],
+                data: [40, 35, 15, 10],
                 backgroundColor: [
                     "rgb(3, 169, 244)",
+                    "rgb(255, 87, 34)",
                     "rgb(38, 166, 154)",
-                    "rgb(255, 235, 59)",
-                    "rgb(255, 87, 34)"
+                    "rgb(255, 235, 59)"
                 ],
                 borderWidth:[0,0,0,0]
             }
